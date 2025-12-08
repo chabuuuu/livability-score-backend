b9904d0361545c395f853981d7615968260b3dee
chabuuuu <haphuthinh332004@gmail.com>
2025-12-08 20:50:45 +0700

    build(media service): media service api

---
+++ media_service/src/controllers/media.controller.ts
@@ -0,0 +1,63 @@
+import { MediaService } from "@/services/media.service";
+import BaseError from "@/utils/base.error";
+import { GlobalConfig } from "@/utils/config/global-config.util";
+import { Request, Response, NextFunction } from "express";
+
+export class MediaController {
+  private mediaService: MediaService;
+
+  constructor() {
+    this.mediaService = new MediaService(); // Khởi tạo service
+  }
+
+  async uploadImage(req: Request, res: Response, next: NextFunction) {
+    if (!req.file) {
+      return res.send_badRequest("No file uploaded or file is too large.");
+    }
+
+    try {
+      const tempFilePath = req.file.path;
+
+      const result = await this.mediaService.uploadImage(tempFilePath);
+      res.send_ok("Upload image successfully", result);
+    } catch (error) {
+      throw new BaseError("UNKNOWN", "Upload image failed");
+    }
+  }
+
+  async uploadVideo(req: Request, res: Response, next: NextFunction) {
+    if (!req.file) {
+      return res.send_badRequest("No file uploaded or file is too large.");
+    }
+    if (!req.params.fileName) {
+      return res.send_badRequest("No file name provided.");
+    }
+
+    try {
+      const tempFilePath = req.file.path;
+      const fileName = req.params.fileName;
+
+      const bucketName = GlobalConfig.media_service.video_bucket.path;
+
+      const result = {
+        mediaUrl:
+          GlobalConfig.media_service.url + "/" + bucketName + "/" + fileName,
+      };
+
+      this.mediaService.uploadVideo(fileName, tempFilePath);
+
+      res.send_ok("Upload video successfully", result);
+    } catch (error) {
+      throw new BaseError("UNKNOWN", "Upload video failed");
+    }
+  }
+
+  async getVideoUrl(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await this.mediaService.getVideoUrl();
+      res.send_ok("Get video url successfully", result);
+    } catch (error) {
+      throw new BaseError("UNKNOWN", "Get video url failed");
+    }
+  }
+}
---
+++ media_service/src/services/media.service.ts
@@ -0,0 +1,59 @@
+import { GetMediaUrlRes } from "@/dto/get-image-url.res";
+import { MediaUploadRes } from "@/dto/media-upload.res";
+import { GlobalConfig } from "@/utils/config/global-config.util";
+import minioClient from "@/utils/minio-instance.util";
+import fs from "fs";
+import { v4 as uuidv4 } from "uuid";
+
+export class MediaService {
+  async uploadImage(tempFilePath: string): Promise<MediaUploadRes> {
+    const bucketName = GlobalConfig.media_service.image_bucket.path;
+
+    // Generate a unique file name
+    const fileName = uuidv4();
+
+    await minioClient.fPutObject(bucketName, fileName, tempFilePath);
+
+    // Xóa file tạm sau khi upload
+    fs.unlink(tempFilePath, (unlinkErr) => {
+      if (unlinkErr) {
+        console.error("Error deleting temp file:", unlinkErr);
+      }
+    });
+
+    return {
+      mediaUrl:
+        GlobalConfig.media_service.url + "/" + bucketName + "/" + fileName,
+    };
+  }
+
+  async getVideoUrl(): Promise<GetMediaUrlRes> {
+    const bucketName = GlobalConfig.media_service.video_bucket.path;
+    const fileName = uuidv4();
+    return {
+      mediaUrl:
+        GlobalConfig.media_service.url + "/" + bucketName + "/" + fileName,
+      fileName: fileName,
+    };
+  }
+
+  async uploadVideo(
+    fileName: string,
+    tempFilePath: string
+  ): Promise<MediaUploadRes> {
+    const bucketName = GlobalConfig.media_service.video_bucket.path;
+    await minioClient.fPutObject(bucketName, fileName, tempFilePath);
+
+    // Xóa file tạm sau khi upload
+    fs.unlink(tempFilePath, (unlinkErr) => {
+      if (unlinkErr) {
+        console.error("Error deleting temp file:", unlinkErr);
+      }
+    });
+
+    return {
+      mediaUrl:
+        GlobalConfig.media_service.url + "/" + bucketName + "/" + fileName,
+    };
+  }
+}
